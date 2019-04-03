80531ca1d236e97756540934fa14c9d509a55232
phithu <thu.nguyen@newoceaninfosys.com>
2019-04-03 18:03:56 +0700

    example

---
+++ example/android/app/src/main/java/com/rnloginzalo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.rnloginzalo;
+
+import android.content.Intent;
+
+import com.zing.zalo.zalosdk.oauth.ZaloSDK;
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "RNLoginZalo";
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ZaloSDK.Instance.onActivityResult(this, requestCode, resultCode, data);
+    }
+}
---
+++ example/android/app/src/main/java/com/rnloginzalo/MainApplication.java
@@ -0,0 +1,51 @@
+package com.rnloginzalo;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.zing.zalo.zalosdk.oauth.ZaloSDKApplication;
+
+import rnzalo.RNZaloPackage;
+
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new RNZaloPackage()
+            );
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+    };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ZaloSDKApplication.wrap(this);
+        SoLoader.init(this, /* native exopackage */ false);
+    }
+}
